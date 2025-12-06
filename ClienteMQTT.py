3136d1ed865ade97e431d94cfc399f2e52276ebf
Julio Martinez <julio.martinez.q@ucb,edu.bo>
2025-12-06 19:12:50 -0400

    Agregando cliente MQTT y entorno virtual

---
+++ ClienteMQTT.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+
+broker = "localhost"
+port = 1883
+topic = "casa/piso1/ventanas"
+topic2 = "casa/piso1/puertas"
+
+def on_connect(client, userdata, flags, rc):
+    print("Conectado con el código de resultado: " + str(rc))
+    ventanas = "open"
+    client.publish(topic, str(ventanas))
+    puertas = "closed"
+    client.publish(topic2, str(puertas))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect(broker, port, 60)
+
+client.loop_forever()
